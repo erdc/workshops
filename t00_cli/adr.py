5e9dcd4af6fa5c51f1f113faabd68aecd1596051
cekees <cekees@gmail.com>
2016-04-12 11:39:10 -0500

    added command  line adr example

--- t00_cli/adr.py
+++ t00_cli/adr.py
@@ -1,11 +1,18 @@
-from proteus.TransportCoefficients import *
+"""
+Define the coefficients for and advection-diffusion equation
+"""
+
+from proteus.TransportCoefficients import TC_base
 
 class LAD(TC_base):
     """
     The coefficients of the linear advection-diffusion equation
     """
     def __init__(self,M,A,B):
-        TC_base.__init__(self, 
+        """
+        Initialize the operators for a single component
+        """
+        TC_base.__init__(self,
                          nc=1, #number of components
                          variableNames=['u'],
                          mass      = {0:{0:'linear'}},
@@ -16,9 +23,12 @@ class LAD(TC_base):
         self.M=M;
         self.A=A;
         self.B=B;
-    
+
     def evaluate(self,t,c):
-        c[('m',0)][:]         = self.M*c[('u',0)]  
+        """
+        Evaluation the coefficients
+        """
+        c[('m',0)][:]         = self.M*c[('u',0)]
         c[('dm',0,0)][:]      = self.M
         c[('f',0)][...,0]     = self.B[0]*c[('u',0)]
         c[('f',0)][...,1]     = self.B[1]*c[('u',0)]
--- t00_cli/ladr_2d_p.py
+++ t00_cli/ladr_2d_p.py
@@ -1,10 +1,13 @@
-from proteus import *
 from proteus.default_p import *
-from adr import *
+from proteus.Domain import RectangularDomain
+from adr import LAD
 
 name = "ladr_2d"
 nd = 2; #Two dimensions
-L=(1.0,1.0,1.0); 
+
+domain = RectangularDomain(L=[1.0,1.0],
+                           x=[0.0,0.0])
+
 T=1.0
 
 coefficients=LAD(M=1.0,
@@ -12,15 +15,49 @@ coefficients=LAD(M=1.0,
                     [0.0,0.001]],
                  B=[2.0,1.0])
 
-def getDBC(x,flag):
-    if x[0] == 0.0 or x[1] == 0.0:
+#set boundary conditions based on coordinates
+test_eps = 1.0e-8
+def on_inflow(x, flag):
+    if x[0] < test_eps or x[1] < test_eps:
+        return True
+    else:
+        return False
+
+def on_outflow(x, flag):
+    if (not  on_inflow and
+        (x[0] > L[0] - test_eps or
+         x[1] > L[1] - test_eps)):
+        return True
+    else:
+        return False
+
+#set boundary conditions based on flags
+# def on_inflow(x, flag):
+#     if flag in [domain.boundaryTags['left'],
+#                 domain.boundaryTags['bottom']]:
+#         return True
+#     else:
+#         return False
+
+# def on_outflow(x, flag):
+#     if flag in [domain.boundaryTags['right'],
+#                 domain.boundaryTags['top']]:
+#         return True
+
+def getDirichletBC(x, flag):
+    if on_inflow(x, flag):
         return lambda x,t: 1.0
-    elif x[0] == 1.0 or x[1] == 1.0:
+
+def getNeumannBC_advective(x, flag):
+    return None
+
+def getNeumannBC_diffusive(x, flag):
+    if on_outflow(x, flag):
         return lambda x,t: 0.0
 
-dirichletConditions = {0:getDBC}
-advectiveFluxBoundaryConditions = {}
-diffusiveFluxBoundaryConditions = {0:{}}
+dirichletConditions = {0:getDirichletBC}
+advectiveFluxBoundaryConditions = {0:getNeumannBC_advective}
+diffusiveFluxBoundaryConditions = {0:{0:getNeumannBC_diffusive}}
 
 class IC:
     def __init__(self):
